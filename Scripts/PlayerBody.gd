6e6be2faccb8c8f20ab07dd6f42b2e1fc88b541e
Geegaz <geegazrd@gmail.com>
2021-04-24 19:18:12 +0200

    Added snake-like movement

--- Scripts/PlayerBody.gd
+++ Scripts/PlayerBody.gd
@@ -1,5 +1,40 @@
 extends KinematicBody2D
 
+onready var manager = get_parent()
+
+var last_dir = Vector2.RIGHT
+var new_dir = Vector2.RIGHT
+
+var parts = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func _physics_process(delta):
+	pass
+
+func _input(event):
+	if event.is_action_pressed("ui_right"):
+		new_dir = Vector2.RIGHT
+	if event.is_action_pressed("ui_left"):
+		new_dir = Vector2.LEFT
+	if event.is_action_pressed("ui_up"):
+		new_dir = Vector2.UP
+	if event.is_action_pressed("ui_down"):
+		new_dir = Vector2.DOWN
+
+func move():
+	if $RayCast2D.is_colliding():
+		return
+	if is_zero_approx(last_dir.dot(new_dir)):
+		last_dir = new_dir
+		manager._Line.add_point(position, 1)
+	
+	position = position + last_dir*manager.CELLSIZE
+
+func add_part():
+	pass
+
+func remove_part(i: int):
+	pass
--- Scripts/PlayerManager.gd
+++ Scripts/PlayerManager.gd
@@ -1,5 +1,15 @@
 extends Node
 
+onready var _Player = $PlayerBody
+onready var _Line = $PlayerLine
+
+export var CELLSIZE: Vector2 = Vector2(16, 16)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+	
+
+func _on_MainTimer_timeout():
+	_Player.move()
+
