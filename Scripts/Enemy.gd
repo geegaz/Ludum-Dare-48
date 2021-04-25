e208253e58400a23b7813665b22106c32e36bebb
Valren Starlord <couturier.valentin@bbox.fr>
2021-04-25 14:23:15 +0200

    ça marche vite fait

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -2,21 +2,51 @@ extends KinematicBody2D
 
 class_name Enemy
 
+const CELLSIZE = Vector2(16, 16)
+var canMove = false
+var direction = Vector2.RIGHT
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-var speed = 100
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var timer = get_parent().get_parent().get_node("MainTimer")
+	if timer:
+		timer.connect("timeout", self, "_on_MainTimer_timeout")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
-	if(!get_node("RayCast2D").is_colliding()):
-		var velocity = Vector2()
-		velocity.x += 1
-		velocity = velocity.normalized() * speed
-		velocity = move_and_slide(velocity)
+	pass
+
+func _on_MainTimer_timeout():
+	move()
+	
+func move():
+	if(canMove):
+		canMove = false
+		if(!get_node("RayCast2D").is_colliding()):
+			if !test_move(self.transform, direction*CELLSIZE):
+				position += direction*CELLSIZE
+		elif(get_node("RayCast2D").is_colliding()):
+			randomize()
+			match direction:
+				Vector2.RIGHT:
+					var dirs = [Vector2.UP, Vector2.LEFT, Vector2.DOWN]
+					direction = dirs[randi() % dirs.size()]
+					rotation = direction.angle()
+				Vector2.UP:
+					var dirs = [Vector2.RIGHT, Vector2.LEFT, Vector2.DOWN]
+					direction = dirs[randi() % dirs.size()]
+					rotation = direction.angle()
+				Vector2.LEFT:
+					var dirs = [Vector2.UP, Vector2.RIGHT, Vector2.DOWN]
+					direction = dirs[randi() % dirs.size()]
+					rotation = direction.angle()
+				Vector2.DOWN:
+					var dirs = [Vector2.UP, Vector2.LEFT, Vector2.RIGHT]
+					direction = dirs[randi() % dirs.size()]
+					rotation = direction.angle()
+	else:
+		canMove = true
